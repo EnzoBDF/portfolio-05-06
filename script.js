4a7ef0867bf74cd29763458b5f1ab899e97516ad
unknown <enzobatista24@gmail.com>
2023-10-18 22:23:03 -0300

    ajustes

---
+++ script.js
@@ -0,0 +1,24 @@
+function canvaHumanas(){
+    window.open('https://www.canva.com/design/DAFwZDdtERM/SlUULyBSOaGqzVMI8vFbfQ/edit?utm_content=DAFwZDdtERM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank')
+}
+function canvaMatematica(){
+    window.open('https://www.canva.com/design/DAFxnmniQ5E/T97dslRJWWm8zs-LD50Hmg/edit?utm_content=DAFxnmniQ5E&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton','_blank')
+}
+function canvaLinguagens(){
+    window.open('https://www.canva.com/design/DAFxnIBXbSw/NPRADHo9f5qLV7YdHW8wNA/edit?utm_content=DAFxnIBXbSw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton','_blank')
+}
+function canvaNatureza(){
+    window.open('https://www.canva.com/design/DAFwZBkBtEM/FbClMZf6YjFMJbuLuRf0Yw/edit?utm_content=DAFwZBkBtEM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton','_blank')
+}
+
+function linkedin(){
+    window.open('https://www.linkedin.com/in/enzobatista/','_blank')
+}
+
+function instagram(){
+    window.open('https://www.instagram.com/enzobdf/','_blank')
+}
+
+function github(){
+    window.open('https://github.com/EnzoBDF','_blank')
+}
